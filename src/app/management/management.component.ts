6c2903c40619757b7198f550a02649f571aee9fe
Marten Schwarzmann <marten.schwarzmann@gmx.de>
2020-11-22 21:57:19 +0100

    Initial ManagementPage

--- src/app/management/management.component.ts
+++ src/app/management/management.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+
+export interface Class {
+  externalId: string;
+  name: string;
+  grade: number;
+  numberStudents: number;
+}
+
+const ELEMENT_DATA: Class[] = [
+  {externalId: 'be6d908a-4dfb-4ab6-a6f3-f2fe8f6eae55', name: '7a', grade: 7, numberStudents: 24},
+  {externalId: 'ad4bb066-8483-4298-8f3f-272c9674437a', name: '8b', grade: 8, numberStudents: 21},
+];
 
 @Component({
   selector: 'app-management',
@@ -7,9 +20,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagementComponent implements OnInit {
 
-  constructor() { }
+  public createClassFormGroup: FormGroup | undefined;
+
+  public createNewClassMode = false;
+
+  public displayedColumns: string[] = ['className', 'numberStudents'];
+  public dataSource = ELEMENT_DATA;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createClassFormGroup = this.fb.group({
+      className: [null, Validators.required],
+      students: this.fb.array([]),
+    });
   }
 
+  public createNewClass() {
+    console.log('TODO: Erstelle neue Klasse...');
+  }
+
+  public get classNameFormControl(): any {
+    return this.createClassFormGroup.get('className');
+  }
 }
--- src/app/management/management.module.ts
+++ src/app/management/management.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ManagementRoutingModule } from './management-routing.module';
 import { ManagementComponent } from './management.component';
 import { MaterialModule } from '../shared/modules/material-module';
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [ManagementComponent],
-  imports: [CommonModule, ManagementRoutingModule, MaterialModule]
+  imports: [CommonModule, ManagementRoutingModule, MaterialModule, ReactiveFormsModule]
 })
 export class ManagementModule {}
