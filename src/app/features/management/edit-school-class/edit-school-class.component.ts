ef3265a13bddc87bd73b61962228d63591fe7edc
Marten Schwarzmann <marten.schwarzmann@gmx.de>
2020-12-20 00:07:33 +0100

    Adds table to editSchool component

--- src/app/features/management/edit-school-class/edit-school-class.component.ts
+++ src/app/features/management/edit-school-class/edit-school-class.component.ts
@@ -5,6 +5,8 @@ import { UserState } from '../../../shared/user/store/user.state';
 import { combineLatest, Observable } from 'rxjs';
 import { SchoolClass } from '../../../shared/user/models/school-class';
 import { distinctUntilChanged } from 'rxjs/operators';
+import { Student } from '../../../shared/user/models/student';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-edit-school-class',
@@ -13,10 +15,15 @@ import { distinctUntilChanged } from 'rxjs/operators';
 })
 export class EditSchoolClassComponent implements OnInit {
   public selectedExternalSchoolClassId: string;
+
   public selectedSchoolClass: SchoolClass;
+
+  public dataSource: MatTableDataSource<Student>;
 
   @Select(UserState.getAvailableSchoolClasses)
   public availableSchoolClasses$: Observable<SchoolClass[]>;
+
+  public displayedColumns: string[] = ['firstName', 'lastName', 'action'];
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
@@ -33,9 +40,21 @@ export class EditSchoolClassComponent implements OnInit {
           for (const availableSchoolClass of availableSchoolClasses) {
             if (this.selectedExternalSchoolClassId === availableSchoolClass.externalId) {
               this.selectedSchoolClass = availableSchoolClass;
+              this.dataSource = new MatTableDataSource<Student>(availableSchoolClass.students);
             }
           }
         }
       });
+  }
+
+  public addStudent(firstName: string, lastName: string): void {
+    console.log('Should add new student ' + firstName + ' ' + lastName);
+    console.log('Should dispatch action for creating a new student');
+    this.dataSource._updateChangeSubscription();
+  }
+
+  public editStudent(student: Student): void {
+    console.log('Dialog should open with editing Student ' + student.firstName + ' ' + student.lastName);
+    this.dataSource._updateChangeSubscription();
   }
 }
