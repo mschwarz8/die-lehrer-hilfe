b75e95948ba8ade68ee7c7cce88314383946bda1
Marten Schwarzmann <marten.schwarzmann@gmx.de>
2020-12-19 23:39:58 +0100

    Get schoolClass from store via externalSchoolClassId

--- src/app/features/management/edit-school-class/edit-school-class.component.ts
+++ src/app/features/management/edit-school-class/edit-school-class.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Select } from '@ngxs/store';
+import { UserState } from '../../../shared/user/store/user.state';
+import { combineLatest, Observable } from 'rxjs';
+import { SchoolClass } from '../../../shared/user/models/school-class';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-school-class',
@@ -8,6 +13,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class EditSchoolClassComponent implements OnInit {
   public selectedExternalSchoolClassId: string;
+  public selectedSchoolClass: SchoolClass;
+
+  @Select(UserState.getAvailableSchoolClasses)
+  public availableSchoolClasses$: Observable<SchoolClass[]>;
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
@@ -15,5 +24,18 @@ export class EditSchoolClassComponent implements OnInit {
     this.activatedRoute.params.subscribe((params: Params) => {
       this.selectedExternalSchoolClassId = params.externalSchoolClassId;
     });
+
+    combineLatest([this.activatedRoute.params, this.availableSchoolClasses$])
+      .pipe(distinctUntilChanged())
+      .subscribe(([params, availableSchoolClasses]) => {
+        this.selectedExternalSchoolClassId = params.externalSchoolClassId;
+        if (!!this.selectedExternalSchoolClassId && !! availableSchoolClasses && availableSchoolClasses.length !== 0) {
+          for (const availableSchoolClass of availableSchoolClasses) {
+            if (this.selectedExternalSchoolClassId === availableSchoolClass.externalId) {
+              this.selectedSchoolClass = availableSchoolClass;
+            }
+          }
+        }
+      });
   }
 }
